3edeef89c00baef52e968786910489a04c0e833f
Inger <inger@Susannas-MacBook-Air.local>
2018-09-19 21:00:35 +0200

    Removed plan in plan

--- src/main/java/se/frihak/ticketplanner/biljett/Biljettplan.java
+++ src/main/java/se/frihak/ticketplanner/biljett/Biljettplan.java
@@ -11,11 +11,10 @@ import se.frihak.ticketplanner.kalender.Dag;
 import se.frihak.ticketplanner.kalender.Resa;
 
 public class Biljettplan extends TicketplannerBase {
-	private Biljettplan plan = null;
 	private List<Biljett> biljetter = new ArrayList<Biljett>();
 
 	public Biljettplan(Biljettplan biljettplan, Biljett bilj) {
-		plan = biljettplan;
+		biljetter.addAll(biljettplan.biljetter);
 		biljetter.add(bilj);
 	}
 
@@ -36,54 +35,12 @@ public class Biljettplan extends TicketplannerBase {
 			}
 		}
 
-		// Kolla om denna plan redan har en giltig plan. Om ja, så returneras denna plan
-		if (plan != null && plan.isValid(resa)) {
-			plan.add(resa);
-			nyaPlaner.add(this);
-			return nyaPlaner;
-		}
-
 		// Skapa nya planer, en för varje biljetttyp
 		for (Biljett oneTicket : ticketcreator.createAllTickets(resa)) {
 			nyaPlaner.add(new Biljettplan(this, oneTicket));
 		}
 
 		return nyaPlaner;
-	}
-
-	private void add(Resa resa) {
-
-		// Skapa en testmetod JUnit om denna blir public
-
-		if (biljetter.size() > 0) {
-			Biljett biljett = biljetter.get(biljetter.size() - 1);
-
-			if (biljett.isValid(resa)) {
-				biljett.add(resa);
-				return;
-			}
-		}
-
-		if (plan != null) {
-			plan.add(resa);
-		}
-	}
-
-	private boolean isValid(Resa resa) {
-
-		// Skapa en testmetod JUnit om denna blir public
-
-		if (biljetter.size() > 0) {
-			Biljett biljett = biljetter.get(biljetter.size() - 1);
-			return biljett.isValid(resa);
-		}
-
-		// Kolla om denna plan redan har en giltig plan. Om ja, så returneras denna plan
-		if (plan != null) {
-			return plan.isValid(resa);
-		}
-
-		return false;
 	}
 
 	@Override
@@ -94,9 +51,6 @@ public class Biljettplan extends TicketplannerBase {
 	@Override
 	public String toString() {
 		StringBuffer buf = new StringBuffer();
-		if (plan != null) {
-			buf.append(plan);
-		}
 		for (Iterator<Biljett> iter = biljetter.iterator(); iter.hasNext();) {
 			Biljett biljett = iter.next();
 			buf.append(biljett);
@@ -122,10 +76,6 @@ public class Biljettplan extends TicketplannerBase {
 			totalpris += bilj.getPris();
 		}
 
-		if (plan != null) {
-			totalpris += plan.getPrice();
-		}
-
 		return totalpris;
 	}
 
@@ -136,22 +86,12 @@ public class Biljettplan extends TicketplannerBase {
 			// Returnera sista biljettens sista giltighetsdag
 			Biljett biljett = biljetter.get(biljetter.size() - 1);
 			sistaDag = biljett.getSistaGiltighetsdag();
-		} else if (plan != null) {
-			// Om det finns en plan så returnera dess sista dag.
-			sistaDag = plan.getSistaGiltighetsdag();
-		} else {
-			// Varken biljett eller plan finns, returnera null
 		}
 
 		return sistaDag;
 	}
 
 	public void write(PrintStream p) {
-		// TODO Skapa en testmetod JUnit
-
-		if (plan != null) {
-			plan.write(p);
-		}
 		for (Iterator<Biljett> iter = biljetter.iterator(); iter.hasNext();) {
 			Biljett biljett = iter.next();
 			biljett.write(p);
