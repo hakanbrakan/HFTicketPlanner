38be6f5404f193e1fb3f8e8916438a64cc0fd8e7
Inger <inger@Susannas-MacBook-Air.local>
2018-11-12 21:39:40 +0100

    Reportfile as temporary file in tests

--- src/test/java/se/frihak/ticketplanner/HFTicketplannerTest.java
+++ src/test/java/se/frihak/ticketplanner/HFTicketplannerTest.java
@@ -2,37 +2,53 @@ package se.frihak.ticketplanner;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.LogManager;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class HFTicketplannerTest {
+
+	@BeforeClass
+    public static void beforeClass() {
+    		System.setProperty("java.util.logging.config.file", ClassLoader.getSystemResource("logging.properties").getPath());
+		reReadLogManager();
+    }
+    
 	@Test
 	public void testMainFunctionOnFiles_Stor() throws IOException {
-		HFTicketplanner.main(new String[] { getClass().getResource("/HFticketplanner_20180820.properties").getPath() });
+		File tempFile = getTempfile();
+		
+		HFTicketplanner.main(new String[] { getClass().getResource("/HFticketplanner_20180820.properties").getPath(), tempFile.getPath()});
 
-		final List<String> linesActual = Files.readAllLines(Paths.get("/private/tmp/Report.txt"));
+		final List<String> linesActual = Files.readAllLines(tempFile.toPath());
 		final List<String> linesExpected = Files.readAllLines(Paths.get(getClass().getResource("/HFTicketplannerReport_20180820.txt").getPath()));
 		assertEquals(linesExpected, linesActual);
 	}
 
 	@Test
 	public void testMainFunctionOnFiles_Liten() throws IOException {
-		HFTicketplanner.main(new String[] { getClass().getResource("/HFticketplanner_20180827.properties").getPath() });
+		File tempFile = getTempfile();
 
-		final List<String> linesActual = Files.readAllLines(Paths.get("/private/tmp/Report.txt"));
+		HFTicketplanner.main(new String[] { getClass().getResource("/HFticketplanner_20180827.properties").getPath(), tempFile.getPath()});
+
+		final List<String> linesActual = Files.readAllLines(tempFile.toPath());
 		final List<String> linesExpected = Files.readAllLines(Paths.get(getClass().getResource("/HFTicketplannerReport_20180827.txt").getPath()));
 		assertEquals(linesExpected, linesActual);
 	}
 
 	@Test
 	public void testMainFunctionOnFiles_Gammal() throws IOException {
-		HFTicketplanner.main(new String[] { getClass().getResource("/HFticketplanner_20150911.properties").getPath() });
+		File tempFile = getTempfile();
 
-		final List<String> linesActual = Files.readAllLines(Paths.get("/private/tmp/Report.txt"));
+		HFTicketplanner.main(new String[] { getClass().getResource("/HFticketplanner_20150911.properties").getPath(), tempFile.getPath()});
+
+		final List<String> linesActual = Files.readAllLines(tempFile.toPath());
 		final List<String> linesExpected = Files.readAllLines(Paths.get(getClass().getResource("/HFTicketplannerReport_20150911.txt").getPath()));
 		assertEquals(linesExpected, linesActual);
 	}
@@ -40,5 +56,20 @@ public class HFTicketplannerTest {
 	@Test
 	public void justToImproveCodeCoverage() throws IOException {
 		new HFTicketplanner();
+	}
+
+	private static void reReadLogManager() {
+		LogManager lm = LogManager.getLogManager();
+		try {
+			lm.readConfiguration();
+		} catch (SecurityException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private File getTempfile() throws IOException {
+		File tempFile = File.createTempFile("HFTicketplanner-", ".tmp");
+		tempFile.deleteOnExit();
+		return tempFile;
 	}
 }
